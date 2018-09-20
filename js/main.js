65f8785ef789f0298d893fbca1133f864a301552
Ryan Dulac <analyticrye@gmail.com>
2018-09-19 23:26:25 -0400

    project started

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+
+
+$(document).ready(function () {
+
+    //get the current year for copyright
+    $('#year').text(new Date().getFullYear());
+    //init scrollspy
+    $('body').scrollspy({ target: '#main-nav' });
+
+    //smooth scrolling
+    $('#main-nav a').on('click', function (event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+});
