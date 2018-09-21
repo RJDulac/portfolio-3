89339c5f2a834b7edd4290ab95d7c3f7f324f7bc
Ryan Dulac <analyticrye@gmail.com>
2018-09-21 04:08:23 -0400

    card slider done

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
     $('body').scrollspy({ target: '#main-nav' });
 
     //smooth scrolling
-    $('#main-nav a').on('click', function (event) {
+    $('#main-nav a, #home-section a').on('click', function (event) {
         if (this.hash !== '') {
             event.preventDefault();
 
